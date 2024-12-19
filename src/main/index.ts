f5da038de359a1d21113ef3afa3784f77e679111
suulnnka <suulnnka@gmail.com>
2024-12-19 15:09:14 +0800

    init

---
+++ src/main/index.ts
@@ -0,0 +1,34 @@
+import { app, BrowserWindow } from 'electron';
+import * as path from 'path';
+
+let mainWindow: BrowserWindow;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+        },
+    });
+
+    mainWindow.loadFile('src/renderer/index.html');
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
