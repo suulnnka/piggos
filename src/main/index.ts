59925868a34386e31d236a1ca34f49e4ccb390ca
suulnnka <suulnnka@gmail.com>
2025-01-04 18:17:31 +0800

    add addr ops

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,4 +1,4 @@
-import { app, BaseWindow, WebContentsView } from 'electron'
+import { app, BaseWindow, WebContentsView, ipcMain } from 'electron'
 import { join } from 'path'
 import { electronApp, is } from '@electron-toolkit/utils'
 
@@ -21,7 +21,11 @@ function createWindow() {
     let leftbar_w = 0
     let rightbar_w = 0
 
-    const parentView = new WebContentsView()
+    const parentView = new WebContentsView({
+        webPreferences: {
+            preload: join(__dirname, '../preload/index.js'),
+        }
+    })
     win.contentView.addChildView(parentView)
 
     parentView.setBounds({ x: 0, y: 0, width: w, height: d })
@@ -41,8 +45,37 @@ function createWindow() {
         height: d-topbar_h-2*bound
     })
 
-    browserView.webContents.loadURL('https://kimi.moonshot.cn')
-    //browserView.webContents.loadURL('https://www.doubao.com/chat/search')
+    let navigate = (_,url) => parentView.webContents.send("navigate",url);
+
+    let browser_home_page = 'https://baidu.com';
+    //let browser_home_page = 'https://www.doubao.com/chat/search';
+
+    //parentView.webContents.openDevTools();
+    parentView.webContents.on('did-finish-load', () => {
+        browserView.webContents.loadURL(browser_home_page);
+        navigate(null,browser_home_page);
+    });
+
+    browserView.webContents.on('will-navigate', navigate);
+    browserView.webContents.on('did-navigate', navigate);
+    browserView.webContents.on('did-navigate-in-page', navigate);
+    
+    browserView.webContents.setWindowOpenHandler(({url}) => {
+        // TODO 打开新tab
+        browserView.webContents.loadURL(url);
+        navigate(null,url);
+        return {action: 'deny'};
+    });
+
+    ipcMain.on('browser-go',(_,url)=>{
+        browserView.webContents.loadURL(url)
+    });
+    ipcMain.on('browser-back',()=>{
+        if(browserView.webContents.canGoBack()) browserView.webContents.goBack()
+    })
+    ipcMain.on('browser-next',()=>{
+        if(browserView.webContents.canGoForward()) browserView.webContents.goForward()
+    })
 
     win.on('resize',() => {
         w = win.getContentSize()[0];
